b32018e22f0bc85d3af4a03ffc1905b2d861e4ed
Efren Trejo Montesdeoca <EfrenTrejoMontedesocas@hotmail.com>
2020-03-24 21:01:49 +0000

    conceptos

---
+++ operadores_aritmeticos.js
@@ -0,0 +1,15 @@
+
+//var operadorUno; 
+//var operadorDos;
+//operadorUno = prompt();
+
+var suma = 4+5;
+var resta = 4-5;
+var divicion = 4/5;
+var multiplicacion = 4*5;
+
+
+console.log (suma);
+console.log (resta);
+console.log (divicion);
+console.log (multiplicacion);
